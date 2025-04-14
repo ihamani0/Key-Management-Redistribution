d48aa956b303603311efe9eafb4d5bc080473126
issam eddien HAMANI <83102668+ihamani0@users.noreply.github.com>
2025-04-14 20:02:28 +0100

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -12,21 +12,4 @@ const sequelize = new Sequelize(
   }
 );
 
-export const dbConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("--- Data Base connection  ---:");
-  } catch (error) {
-    console.log("--- Data Base Connection Error  ---:", error);
-  }
-};
-
-export const dbDeconnection = async () => {
-  try {
-    await sequelize.close();
-    console.log("--- Data Base Deconnection  ---:");
-  } catch (error) {
-    console.log("--- Data Base Deconnection Error  ---:", error);
-  }
-};
 export default sequelize;
